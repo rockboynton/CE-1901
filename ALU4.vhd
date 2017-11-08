fc4f93a919592bef58a62cb699a34cf255e0ab54
rockboynton <32227280+rockboynton@users.noreply.github.com>
2017-11-07 23:16:11 -0600

    Update ALU4.vhd

--- ALU4.vhd
+++ ALU4.vhd
@@ -1,16 +1,23 @@
+--Rock Boynton
+--Date: 11/7/17
+--Course: CE 1901-021
+--Professor: Dr. Livingston
+--Purpose: This is the structural VHDL code for a 4-bit Arithmetic Logic Unit (ALU)
+
 library ieee;
 use ieee.std_logic_1164.all;
 
 entity ALU4 is
 	port(
-			A, B : in std_logic_vector(3 downto 0);
-			S : in std_logic_vector(2 downto 0);
-			F : out std_logic_vector(3 downto 0);
-			C, V : out std_logic
+			A, B : in std_logic_vector(3 downto 0); --operands
+			S : in std_logic_vector(2 downto 0); --select, selects which operation to perform 
+			F : out std_logic_vector(3 downto 0); -- 4 bit result of operation
+			C, V : out std_logic --C, carry out bit if any; V, overflow indicator
 	);
 end entity ALU4;
 
-
+--contains a ripple carry adder-subtractor(RCA4) to perform add and subtract operations
+--also contains 4 multiplexars, each outputting a bit of the result, which depends on the selected operation
 architecture STRUCTURAL of ALU4 is 
 
 	component RCA4 is 
@@ -31,9 +38,9 @@ architecture STRUCTURAL of ALU4 is
 		);
 	end component Mux8to1;
 		
-	signal sum : std_logic_vector(3 downto 0);
-	signal c3 : std_logic;
-	signal c4 : std_logic;
+	signal sum : std_logic_vector(3 downto 0); --signals from sum bits of the RCA to the multiplexars
+	signal c3 : std_logic; --second most significant carry bit of the RCA4
+	signal c4 : std_logic; --most significant carry bit of the RCA4 (AKA carry out)
 	
 begin 
 
@@ -45,45 +52,24 @@ begin
 				   S     => sum,
 				   C4    => c4,
 					C3    => c3
-				   --might need C3 connected to something here not sure yet
 				);
 				
 	GEN_MUX: for i in 0 to 3 generate
 		Mux : Mux8to1
 					port map(
-						I(0) => sum(i),
-						I(1) => sum(i),
-						I(2) => A(i) and B(i),
-						I(3) => A(i) or B(i),
-						I(4) => A(i) nand B(i),
-						I(5) => A(i) xor B(i),
-						I(6) => '1',
-						I(7) => '0',
-						S    => S,
-						Y    => F(i)
+						I(0) => sum(i),          -- add
+						I(1) => sum(i),          -- subtract
+						I(2) => A(i) and B(i),   -- bitwise AND
+						I(3) => A(i) or B(i),    -- bitwise OR
+						I(4) => A(i) nand B(i),  -- bitwise NAND
+						I(5) => A(i) xor B(i),   -- bitwise XOR
+						I(6) => '1',             -- outputs all bits to '1'
+						I(7) => '0',             -- outputs all bits to '0'
+						S    => S,               -- selection bits (inputted in circuit)
+						Y    => F(i)             -- output of circuit
 					);
 	end generate GEN_MUX;
 				
---	Mux1 : Mux8to1
---				port map(
---					I => 
---					S =>
---					Y =>
---				)
---				
---	Mux1 : Mux8to1
---				port map(
---					I => 
---					S =>
---					Y =>
---				)
---				
---	Mux1 : Mux8to1
---				port map(
---					I => 
---					S =>
---					Y =>
---				)
 C <= c4;
 V <= c3 xor c4;
 
